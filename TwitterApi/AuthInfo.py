901b33717f7fe4f28c450af82bd8ffac570bfe86
Rajat Sethi <rajats3@uw.edu>
2016-10-16 05:31:31 -0700

    adding twitter api#

---
+++ TwitterApi/AuthInfo.py
@@ -0,0 +1,11 @@
+class AuthInfo(object):
+
+    def __init__(self, consumer_key = "20L3pj0XqJszENU5tVVFrKntT",
+                 consumer_secret = "ptNul7KPR5gidrmfdbzc897f4oEESAebvOpViEU2ZBr8T15dmb",
+                 access_token= "702690933679083520-ZPYrkYT0kfjfXGbO6xcwe6Bth6P9DIN",
+                 access_token_secret = "miZ4ogEVCqokELn7JVky3Qgsmnf8wN8Xb8inEEJU16NJG"):
+        self.consumer_key = consumer_key
+        self.consumer_secret = consumer_secret
+        self.access_token = access_token
+        self.access_token_secret = access_token_secret
+
